9dd8650418c39c7c39419f04483b50c05059bd94
Yuriy <kireev_juriyy@rambler.ru>
2021-06-02 00:26:10 +0800

    add the cod in task_6_6.py

---
+++ add_price.py
@@ -0,0 +1,4 @@
+from task_6_6 import add_price
+import sys
+
+add_price(sys.argv[1])
--- task_6_6.py
+++ task_6_6.py
@@ -1,1 +1,23 @@
+def add_price(arg):
+    with open('bakery.csv', 'a', encoding='UTF-8') as f:
+        f.write(f'{arg}\n')
+
+
+def show_price(interval):
+
+    start = 1 if len(interval) == 1 else int(interval[1])
+    step = 0 if len(interval) < 3 else int(interval[2])
+    with open('bakery.csv', 'r', encoding='UTF-8') as f:
+        file_lines = f.readline().strip()
+        i = 1
+        while file_lines:
+            if i != step:
+                if i >= start:
+                    print(file_lines)
+                    file_lines = f.readline().strip()
+                else:
+                    file_lines = f.readline().strip()
+            else:
+                break
+            i += 1
 
